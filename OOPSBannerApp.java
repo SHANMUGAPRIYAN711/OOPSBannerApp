0a703a7d0d83a6e5167d219e0878ebde1d4af7e7
SHANMUGAPRIYAN711 <shanmugapriyant711@gmail.com>
2026-04-14 18:07:26 +0530

    [SHANMUGAPRIYAN]:UC8- Implemented hashmap based dynamic banner rendering

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,28 +1,64 @@
-class CharacterPattern{
-  char character;
-  String[] pattern;
-  CharacterPattern(char character, String[] pattern){
-    this.character = character;
-    this.pattern = pattern;
-  }
-  public void printPattern(){
-    for(String line : pattern){
-      System.out.println(line);
-    }
-  }
-}
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
+
+    // Create patterns for characters
+    public static Map<Character, String[]> getPatterns() {
+
+        Map<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        });
+
+        map.put('P', new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        });
+
+        map.put('S', new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        });
+
+        return map;
+    }
+
+    // Print banner dynamically
+    public static void printBanner(String text) {
+
+        Map<Character, String[]> patterns = getPatterns();
+
+        int height = 7;
+
+        for (int i = 0; i < height; i++) {
+
+            for (char ch : text.toCharArray()) {
+                System.out.print(patterns.get(ch)[i] + "  ");
+            }
+
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-      String[] pattern = {
-        " ***** ",
-        "*     *",
-        "*     *",
-        "*     *",
-        "*     *",
-        "*     *",
-        " ***** "
-      };
-      CharacterPattern c = new CharacterPattern('O', pattern);
-      c.printPattern();
+        printBanner("OOPS");
     }
 }
